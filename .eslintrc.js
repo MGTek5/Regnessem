bf7572fb0161dac62d2132a7199cc0dbeebb8299
Theo Marinho <theo.marinho@epitech.eu>
2021-10-28 18:14:53 +0200

    [fix] fix build

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,10 @@
- module.exports = {
- extends: ['airbnb', 'airbnb/hooks'],
- env: {
-   browser: true,
-   jest: true,
- },
- rules: {
-   'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
- },
+module.exports = {
+  extends: ['airbnb', 'airbnb/hooks'],
+  env: {
+    browser: true,
+    jest: true,
+  },
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+  },
 };
