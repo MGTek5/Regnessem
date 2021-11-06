3ceb844724a8c491bc785c73afb66c74f4f9eaea
Theo Marinho <theo.marinho@epitech.eu>
2021-11-06 18:26:44 +0100

    linting up tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,18 +5,18 @@ module.exports = {
     extend: {
       colors: {
         yellow: {
-          "DEFAULT": "#dac64e",
-          900: "#dac64ee6",
-          800: "#dac64ecc",
-          700: "#dac64eb3",
-          600: "#dac64e99",
-          500: "#dac64e80",
-          400: "#dac64e66",
-          300: "#dac64e4d",
-          200: "#dac64e33",
-          100: "#dac64e1a",
-        }
-      }
+          DEFAULT: '#dac64e',
+          900: '#dac64ee6',
+          800: '#dac64ecc',
+          700: '#dac64eb3',
+          600: '#dac64e99',
+          500: '#dac64e80',
+          400: '#dac64e66',
+          300: '#dac64e4d',
+          200: '#dac64e33',
+          100: '#dac64e1a',
+        },
+      },
     },
   },
   variants: {
