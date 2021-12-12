d509b37a3f7032dd5557c1b52d18dd8f404a67e3
Theo Marinho <theo.marinho@epitech.eu>
2021-12-12 19:43:17 +0100

    [fix] fix lint

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,16 +5,16 @@ module.exports = {
     extend: {
       colors: {
         pupule: {
-          DEFAULT: "#570df8",
-          900: "#570df8e6",
-          800: "#570df8cc",
-          700: "#570df8b3",
-          600: "#570df899",
-          500: "#570df880",
-          400: "#570df866",
-          300: "#570df84d",
-          200: "#570df833",
-          100: "#570df81a",
+          DEFAULT: '#570df8',
+          900: '#570df8e6',
+          800: '#570df8cc',
+          700: '#570df8b3',
+          600: '#570df899',
+          500: '#570df880',
+          400: '#570df866',
+          300: '#570df84d',
+          200: '#570df833',
+          100: '#570df81a',
         }
       },
     },
@@ -23,9 +23,7 @@ module.exports = {
     extend: {},
   },
   plugins: [
-    // eslint-disable-next-line global-require
     require('daisyui'),
-    // eslint-disable-next-line global-require
     require('tailwindcss-scrollbar')
   ],
 };
