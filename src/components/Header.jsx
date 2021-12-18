198020ecd4e6f1f9f0fb3d089c57461aaa15520f
Theo Marinho <theo.marinho@epitech.eu>
2021-12-18 17:38:46 +0100

    [add] #WIP working on header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,11 +7,23 @@ const Header = () => {
   const location = useLocation().pathname;
   if (NO_HEADER_ROUTES.includes(location)) return null;
   return (
-    <header className="w-full h-16 bg-gray-900 text-white flex items-center justify-between text-xl">
-      <div className="flex justify-evenly w-full">
-        <Link to="/">Link 1</Link>
-        <Link to="/">Link 2</Link>
-        <Link to="/">Link 3</Link>
+    <header className="w-full h-16 bg-gray-900 text-white flex items-center justify-between px-8 text-xl">
+      <Link to="/">
+        <img src="/logo192.png" className="h-8" alt="logo" />
+      </Link>
+      <div className="dropdown dropdown-end dropdown-hover">
+        <div className="btn btn-ghost rounded-btn">Dropdown</div>
+        <ul className="p-2 shadow menu dropdown-content bg-base-100 rounded-box w-52">
+          <li>
+            <Link to="/">Item 1</Link>
+          </li>
+          <li>
+            <Link to="/">Item 2</Link>
+          </li>
+          <li>
+            <Link to="/">Item 3</Link>
+          </li>
+        </ul>
       </div>
     </header>
   );
