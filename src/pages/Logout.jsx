ae5965bfe706e850ec09d0cb8589b987ed4251e0
Theo Marinho <theo.marinho@epitech.eu>
2021-12-28 22:38:23 +0100

    [fix] fix user context and add private routes

--- src/pages/Logout.jsx
+++ src/pages/Logout.jsx
@@ -1,18 +1,22 @@
-import React, { useEffect } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
+import UserContext from '../contexts/user.context';
 
 const Logout = () => {
   const history = useHistory();
+  const userContext = useContext(UserContext);
 
   useEffect(() => {
-    if (history) {
+    if (history && userContext) {
       setTimeout(() => {
         localStorage.removeItem('regnessem-token');
         localStorage.removeItem('regnessem-user');
+        userContext.user = null;
+        userContext.authed = false;
         history.push('/login');
       }, 2500);
     }
-  }, [history]);
+  }, [history, userContext]);
 
   return (
     <div className="h-full w-full flex flex-col items-center justify-center">
