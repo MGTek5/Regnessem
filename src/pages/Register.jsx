8ad5065b77e0a2bf451ab434e759164109f43267
Matteo Gassend <matteo.gassend@epitech.eu>
2021-12-31 10:52:16 +0100

    [fix] consistent style for login & logout

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -55,8 +55,8 @@ const Register = () => {
       />
       <div style={{ background: 'rgba(0,0,0,0.85)' }} className="absolute top-0 left-0 h-screen w-screen" />
       <div className="w-full h-full flex justify-center items-center">
-        <div className="card bordered w-full h-full sm:h-auto sm:w-auto md:w-96">
-          <div className="card-body h-full px-8 py-12 bg-slate-800">
+        <div className="card w-full h-full sm:h-auto sm:w-auto md:w-96">
+          <div className="card-body h-full px-8 py-12 bg-slate-900">
             <h2 className="card-title text-5xl font-bold text-center">{t('register.title')}</h2>
             <p className="text-center">{t('register.subtitle')}</p>
             <form onSubmit={formik.handleSubmit} className="mt-4">
@@ -69,7 +69,7 @@ const Register = () => {
                   <input type="checkbox" onChange={() => setIsAFunBoi((old) => !old)} checked={isAFunBoi} className="checkbox checkbox-primary" />
                 </div>
                 <button type="submit" className="btn btn-primary">{t('button.submit')}</button>
-                <Link to="/login" className="capitalize text-right">{t('register.login')}</Link>
+                <Link to="/login" className="capitalize text-right hover:underline">{t('register.login')}</Link>
               </div>
             </form>
           </div>
