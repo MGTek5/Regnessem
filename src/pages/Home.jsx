3f61291ac5117ec158bcbb40cda195234f41d356
Matteo Gassend <matteo.gassend@epitech.eu>
2021-12-31 12:34:10 +0100

    [refact] moved ChatList to compoenent

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,9 +11,7 @@ import { useTranslation } from 'react-i18next';
 import * as namez from 'namez';
 import toast from 'react-hot-toast';
 import { Howl, Howler } from 'howler';
-import Plus from '../components/images/Plus';
 import NewChatModal from '../components/NewChatModal';
-import Picto from '../components/Picto';
 import {
   GET_USERS,
   GET_CHATS,
@@ -25,10 +23,10 @@ import {
   CHAT_DELETED,
   DELETE_CHAT,
 } from '../utils/graphql';
-import Trash from '../components/images/Trash';
 import UserContext from '../contexts/user.context';
 import GifSearch from './Home/GifSearch';
 import Chat from './Home/Chat';
+import ChatList from './Home/ChatList';
 
 function isInViewport(el) {
   const rect = el.getBoundingClientRect();
@@ -208,44 +206,14 @@ const Home = () => {
         </div>
       </div>
       <div className="w-1/5 flex flex-col shadow-md">
-        <div className="flex w-full p-4 border-b items-center">
-          <button type="button" className="outline-none p-1 flex items-center justify-between w-full" onClick={() => setNewChatModalOpen(true)}>
-            <span className="flex items-center font-bold">{t('home.new')}</span>
-            <Plus className="w-6 h-6 bg-pupule rounded-full p-1" />
-          </button>
-        </div>
-        <div className="pr-3 overflow-y-auto overflow-x-hidden scrollbar-w-1 scrollbar-thumb-rounded-full scrollbar-thumb-gray-400 scrollbar-track-gray">
-          {chats.map((chat) => (
-            <div className="indicator w-full" key={chat._id}>
-              <div
-                className={'flex w-full rounded-xl m-1 focus:outline-none group '.concat(selectedChat === chat._id ? 'bg-pupule-900' : 'hover:bg-pupule-400')}
-              >
-                {notifiedChats.includes(chat._id) && <div className="indicator-item badge badge-secondary" />}
-                <button
-                  type="button"
-                  className="flex items-center pl-2 py-2 w-full text-white outline-none"
-                  onClick={() => handleChatChange(chat._id)}
-                >
-                  <Picto members={chat.members} />
-                  <span className="ml-3">{chat.name}</span>
-                </button>
-                <button
-                  type="button"
-                  className="pr-3 hidden group-hover:block"
-                  onClick={() => {
-                    deleteChat({
-                      variables: {
-                        chatId: chat._id,
-                      },
-                    });
-                  }}
-                >
-                  <Trash className="w-5 h-5" />
-                </button>
-              </div>
-            </div>
-          ))}
-        </div>
+        <ChatList
+          chats={chats}
+          setNewChatModalOpen={setNewChatModalOpen}
+          notifiedChats={notifiedChats}
+          selectedChat={selectedChat}
+          deleteChat={deleteChat}
+          handleChatChange={handleChatChange}
+        />
         <NewChatModal
           users={users}
           isOpen={newChatModalOpen}
